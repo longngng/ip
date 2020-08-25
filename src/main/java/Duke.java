46a18843d6c1c3af1233f1bf94377fc75a28d586
longnn <ngoclong0x@gmail.com>
2020-08-25 22:25:09 +0800

    Add the ability to store text entered and display them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Duke");
-        System.out.println("What can I do for you?");
+        
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         String line;
         Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
 
         while (true) {
             line = in.nextLine();
             if(line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                int i = 0;
+                while (i < tasks.length) {
+                    if(tasks[i] == null) {
+                        break;
+                    }
+                    System.out.println((i+1) + ". " + tasks[i]);
+                    i++;
+                }
             } else {
-                System.out.println(line);
+                tasks[taskCount] = line;
+                System.out.println("added: " + line);
+                taskCount++;
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
