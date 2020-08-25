950beb07053142db0d1e55033cfbf2dbcdf31bf5
longnn <ngoclong0x@gmail.com>
2020-08-25 21:55:30 +0800

    Add Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello I'm Duke");
         System.out.println("What can I do for you?");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            if(line.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
